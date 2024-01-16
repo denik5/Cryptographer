5d49cceb53e05656ef4030cfbe3eaf0c93e96598
denik5 <white90@inbox.ru>
2024-01-16 22:12:54 +0300

    add условий для работы с отрицательными значениями итерации и проверка правильность работы кода

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,7 +1,7 @@
 public class CaesarCipher {
-    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static String encrypt(String message, int key) {
+    public String encrypt(String message, int key) {
         StringBuilder builder = new StringBuilder();
         for (char aChar : message.toCharArray()) {
             int indexPosition = ALPHABET.indexOf(aChar);
@@ -19,20 +19,7 @@ public class CaesarCipher {
         return builder.toString();
     }
 
-    public static void main(String[] args) {
-        String string = "ABadda";
-        String encrypt = encrypt(string, 40);
-        System.out.println(encrypt);
-
-        String decrypt = decrypt(encrypt, 40);
-        System.out.println(decrypt);
-    }
-    //1. найти позицию в альфабете
-    //2. к позиции добавить ключ
-    //3. для новой позиции внутри альфа бета найти символ
-    //4. собрать все в строку
-
-    public static String decrypt(String message, int key) {
+    public String decrypt(String message, int key) {
 
         return encrypt(message, key * -1);
     }
